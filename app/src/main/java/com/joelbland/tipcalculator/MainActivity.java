d9b955fe0dd67a2916bf6cd74d54550c8eba48b8
blandco <jbland@gmail.com>
2022-10-18 18:46:38 -0500

    completes landscape activity

--- app/src/main/java/com/joelbland/tipcalculator/MainActivity.java
+++ app/src/main/java/com/joelbland/tipcalculator/MainActivity.java
@@ -1,95 +1,112 @@
 package com.joelbland.tipcalculator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
 import java.text.NumberFormat;
-import android.text.TextWatcher;
-import android.text.Editable;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String MA = "MainActivity";
+
     private TipCalculator tipCalc;
-    public NumberFormat money = NumberFormat.getCurrencyInstance( );
+    private NumberFormat money = NumberFormat.getCurrencyInstance();
     private EditText billEditText;
     private EditText tipEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.w(MA,"Inside onCreate");
         super.onCreate(savedInstanceState);
-        // setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        Configuration config = getResources().getConfiguration();
+        modifyLayout(config);
 
-        Configuration config = getResources( ).getConfiguration( );
-        modifyLayout( config );
+        tipCalc = new TipCalculator(1f,1f);
+
     }
 
-
-    public void onConfigurationChanged( Configuration newConfig ) {
-        Log.w( "MainActivity", "Inside onConfigurationChanged" );
-        super.onConfigurationChanged( newConfig );
-        modifyLayout( newConfig );
-    }
-
-    public void modifyLayout( Configuration newConfig ) {
-        Log.w( "MainActivity", "Inside modifyLayout" );
-        if( newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE )
-            setContentView( R.layout.activity_main_landscape );
-        else if( newConfig.orientation == Configuration.ORIENTATION_PORTRAIT )
-            setContentView( R.layout.activity_main );
-
-        tipCalc = new TipCalculator( 0.17f, 100.0f );
-        billEditText = ( EditText ) findViewById( R.id.amount_bill );
-        tipEditText = ( EditText ) findViewById( R.id.amount_tip_percent );
-
-        TextChangeHandler tch = new TextChangeHandler( );
-        billEditText.addTextChangedListener( tch );
-        tipEditText.addTextChangedListener(tch);
-    }
-
-    /** Called when the user clicks on the Calculate button */
-    public void calculate( ) {
-        EditText billEditText =
-                ( EditText ) findViewById( R.id.amount_bill );
-        EditText tipEditText =
-                ( EditText ) findViewById( R.id.amount_tip_percent );
-        String billString = billEditText.getText( ).toString( );
-        String tipString = tipEditText.getText( ).toString( );
-
-        TextView tipTextView =
-                ( TextView ) findViewById( R.id.amount_tip );
-        TextView totalTextView =
-                ( TextView ) findViewById( R.id.amount_total );
-        try {
-            // convert billString and tipString to floats
-            float billAmount = Float.parseFloat( billString );
-            int tipPercent = Integer.parseInt( tipString );
-            // update the Model
-            tipCalc.setBill( billAmount );
-            tipCalc.setTip( .01f * tipPercent );
-            // ask Model to calculate tip and total amounts
-            float tip = tipCalc.tipAmount( );
-            float total = tipCalc.totalAmount( );
-            // update the View with formatted tip and total amounts
-            tipTextView.setText( money.format( tip ) );
-            totalTextView.setText( money.format( total ) );
-        } catch( NumberFormatException nfe ) {
-            // pop up an alert view here
+    private void modifyLayout(Configuration config) {
+        Log.w(MA,"Inside modifyLayout");
+        if(config.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            setContentView(R.layout.activity_main_landscape);
+        } else if (config.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            setContentView(R.layout.activity_main);
         }
+
+        billEditText = findViewById(R.id.amount_bill);
+        tipEditText = findViewById(R.id.amount_tip_percent);
+
+        TextChangeHandler tch = new TextChangeHandler();
+        billEditText.addTextChangedListener(tch);
+        tipEditText.addTextChangedListener(tch);
+
     }
+
+    public void calculate() {
+
+        String billString = billEditText.getText().toString();
+        String tipString = tipEditText.getText().toString();
+
+        TextView tipTextView = findViewById(R.id.amount_tip);
+        TextView totalTextView = findViewById(R.id.amount_total);
+
+        try {
+
+            // convert strings to floats
+            float billAmount = Float.parseFloat(billString);
+            int tipPercent = Integer.parseInt(tipString);
+
+            // update Model
+            tipCalc.setBill(billAmount);
+            tipCalc.setTip(.01f * tipPercent);
+
+            // calculate in Model
+            float tip = tipCalc.tipAmount();
+            float total = tipCalc.totalAmount();
+
+            // update View
+            tipTextView.setText(money.format(tip));
+            totalTextView.setText(money.format(total));
+
+        } catch (NumberFormatException nfe) {
+            // alert message
+        }
+
+    }
+
+
     private class TextChangeHandler implements TextWatcher {
-        public void afterTextChanged( Editable e ) {
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
             calculate();
         }
+    }
 
-        public void beforeTextChanged( CharSequence s, int start,
-                                       int count, int after ) {
-        }
-
-        public void onTextChanged( CharSequence s, int start,
-                                   int before, int after ) {
-        }
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        Log.w(MA,"Inside onConfigurationChanged");
+        super.onConfigurationChanged(newConfig);
+        modifyLayout(newConfig);
     }
 }
