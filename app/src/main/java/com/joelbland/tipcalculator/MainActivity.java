002b715719c5cef74c5c136114ab40bc91bb5225
blandco <jbland@gmail.com>
2022-10-18 17:17:32 -0500

    wires model to 1st activity

--- app/src/main/java/com/joelbland/tipcalculator/MainActivity.java
+++ app/src/main/java/com/joelbland/tipcalculator/MainActivity.java
@@ -20,14 +20,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        tipCalc = new TipCalculator( 0.17f, 100.0f );
-        billEditText = ( EditText ) findViewById( R.id.amount_bill );
-        tipEditText = ( EditText ) findViewById( R.id.amount_tip_percent );
+        // setContentView(R.layout.activity_main);
 
-        TextChangeHandler tch = new TextChangeHandler( );
-        billEditText.addTextChangedListener( tch );
-        tipEditText.addTextChangedListener(tch);
         Configuration config = getResources( ).getConfiguration( );
         modifyLayout( config );
     }
@@ -45,6 +39,14 @@ public class MainActivity extends AppCompatActivity {
             setContentView( R.layout.activity_main_landscape );
         else if( newConfig.orientation == Configuration.ORIENTATION_PORTRAIT )
             setContentView( R.layout.activity_main );
+
+        tipCalc = new TipCalculator( 0.17f, 100.0f );
+        billEditText = ( EditText ) findViewById( R.id.amount_bill );
+        tipEditText = ( EditText ) findViewById( R.id.amount_tip_percent );
+
+        TextChangeHandler tch = new TextChangeHandler( );
+        billEditText.addTextChangedListener( tch );
+        tipEditText.addTextChangedListener(tch);
     }
 
     /** Called when the user clicks on the Calculate button */
